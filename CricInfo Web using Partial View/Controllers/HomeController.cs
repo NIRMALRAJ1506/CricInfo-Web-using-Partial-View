2de28fe8e88dce57a5977402e063e5d11087a453
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2023-12-26 16:01:02 +0530

    first commit of CricInfo Partial View

---
+++ CricInfo Web using Partial View/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CricInfo_Web_using_Partial_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CricInfo Web using Partial View/Models/Cricket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CricInfo_Web_using_Partial_View.Models
+{
+    public class Cricket
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
